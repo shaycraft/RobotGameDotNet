bda6e103f65f50150620af5fb00025a5052ec992
Sam Haycraft <shaycraft@metlife.com>
2016-08-01 11:41:52 -0600

    Staring classes

---
+++ robotgamedotnet/Robot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace robotgamedotnet
+{
+    public class Robot
+    {
+        public int hp { get; set; }
+
+    }
+}

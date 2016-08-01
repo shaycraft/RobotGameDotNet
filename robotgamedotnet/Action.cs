95b0a4b67d9acfe48174eb833e6f0b2b4bda3a7b
Sam Haycraft <shaycraft@metlife.com>
2016-08-01 13:23:19 -0600

    refactor action

---
+++ robotgamedotnet/Action.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace robotgamedotnet
+{
+    public enum ActionType { Move = 1, Attack = 2, Suicide = 3, Guard = 4 };
+
+    public class Action
+    {
+        public ActionType Type { get; set; }
+        // only used for move
+        public Point p { get; set; }
+    }
+}
